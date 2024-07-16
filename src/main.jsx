31fd5c9ba6bda1a7fb6537abcb1fd541939b0b1c
tegra10 <tegkit30@gmail.com>
2024-07-17 00:02:11 +0100

    routage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import eruda from "eruda";
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+    <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+eruda.init();
